5451394cb6c12905aa22432a7b082d0c2839bbba
Matheus Rocha <42144731+mrocha98@users.noreply.github.com>
2019-11-06 12:29:35 -0300

    conteúdo da pasta src
    demais arquivos seguem a estrutura padrão gerada pelo react-create-app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+import './styles.css';
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
